1f74096cb77ab7f9865e4b9adab180a07e905fec
sj <sujung033131@gmail.com>
2023-10-04 22:59:35 +0900

    1004 useEffect

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  }
-  // useEffect에 한 번만 실행될 함수를 넣어줌
-  useEffect(iRunOnlyOnce, []);
+  // useEffect function : 코드가 딱 한 번만 실행될 수 있도록 보호해줌
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
