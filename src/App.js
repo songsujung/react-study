ce7db59e8b7e019b90e8495a5260394ac4a01004
sj <sujung033131@gmail.com>
2023-10-05 23:55:39 +0900

    1005 Cleanup function

--- src/App.js
+++ src/App.js
@@ -2,40 +2,37 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-// state를 변경할 때 모든 code들은 항상 다시 실행된다.
-// component 내부의 몇몇 코드는 처음 딱 한 번만 실행되고 다시는 실행되지 않도록 하고 싶을 때에 useEffect를 활용(API를 호출할 때 등)
+// cleanUp function : 컴포넌트가 사라진 후에 실행될 함수를 지정하는 것이며 반환되는 함수가 클린업함수이며, 메모리 누수 방지를 위해 사용(자주 사용하지 않는 방식)
+// 버튼을 눌러서 함수가 실행되면서 버튼이 사라지면 'destroyed :('라는 메시지를 콘솔에 출력(리턴)
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  // 이 코드보다 위의 방식으로 사용하는 것이 좋음
+  // useEffect(function(){
+  //   console.log("hi :)");
+  //   return function() {
+  //     console.log("bye :(");
+  //   }
+  // }, []);
+  return <h1>Hello</h1>;
+}
+// function Hello() {
+//   useEffect(() => {
+//     console.log("created :)");
+//     return () => console.log("destroyed :(") // 이 부분을 cleanUp함수라고 함
+//   }, []);
+//   return <h1>Hello</h1>;
+// }
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  // useEffect function : 코드가 딱 한 번만 실행될 수 있도록 보호해줌
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  // keyword가 변화할 때만 코드가 실행 (버튼 누를 때에 검색input이 실행되는 것을 방지)
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  // counter가 변화할 때만 코드가 실행
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  // keyword, counter 중에서 하나라도 변화하면 코드가 실행
-  useEffect(() => {
-    console.log("I run when 'keyword' & 'counter' changes.");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input 
-        value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
