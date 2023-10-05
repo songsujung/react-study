ccf8ce108d4c6a8a88785a1e1b07002bf3b4e615
sj <sujung033131@gmail.com>
2023-10-05 23:57:31 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-// cleanUp function : 컴포넌트가 사라진 후에 실행될 함수를 지정하는 것이며 반환되는 함수가 클린업함수이며, 메모리 누수 방지를 위해 사용(자주 사용하지 않는 방식)
+// cleanUp function : 컴포넌트가 사라진 후에 실행될 함수를 지정하는 것이며 반환되는 함수가 클린업함수이다. 메모리 누수 방지를 위해 사용(자주 사용하지 않는 방식)
 // 버튼을 눌러서 함수가 실행되면서 버튼이 사라지면 'destroyed :('라는 메시지를 콘솔에 출력(리턴)
 function Hello() {
   useEffect(() => {
