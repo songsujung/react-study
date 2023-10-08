8f083e886dab9327b6400734271fb9213c2307fb
sj <sujung033131@gmail.com>
2023-10-08 23:36:12 +0900

    1008 암호화폐와 가격나열 (useEffect 연습)

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import { useState, useEffect } from "react";
 
-// Todo 만들기
-// 갖고 있는 array를 직접적으로 수정하지 않으면서 한가지를 더 추가. toDos.push or toDos=""같이 State를 직접적으로 수정X, 수정하는 함수 생성
+// 암호화폐와 그 가격을 나열
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]); // 여러 개의 toDo를 받을 수 있는 array 생성
-  const onChange = (event) => setToDo(event.target.value); // toDo값을 수정하는 함수
-  const onSubmit = (event) => {
-    event.preventDefault();
-    // toDo가 비어있다면 리턴
-    if(toDo === "") {
-      return;
-    }
-    // setTodos 안에 함수를 넣어서 array를 수정
-    // 현재상태:currnentArray, []새로운 배열안에 toDo, ...currentArray 담음 (...을 쓰면 현재상태에 입력한toDo가 추가되는 배열 생성됨, ...을 안하면 배열안에 배열이 추가됨)
-    setToDos(currentArray => [toDo, ...currentArray]);
-    // setToDo 함수를 불러와서 input 비우기
-    setToDo("");
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]); // 기본 값으로 빈 배열을 넘겨주어 coin array를 0으로 시작, 아무 것도 설정을 안해주면 coins가 undefined이 되어 에러가 난다.
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json())
+      .then((json) => {
+        setCoins(json)
+        setLoading(false);
+      });
+  }, []); // 해당 Effect를 한 번만 사용할 거기때문에 어떤 것에도 의존하지 않고 []로 비워둠(빈 배열이면 한 번만 작동)
   return (
-    // toDos.map함수 : array를 가져와서, 이 array의 item들을 변형해서 li가 되도록 만듦(입력하는 todo마다 li로 만들어 array를 생성)
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange} 
-          value={toDo} 
-          type="text" 
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+        {coins.map((coin) => (
+          <li>{coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD</li>
         ))}
-      </ul>
+        </ul>
+      )}
+
     </div>
   );
 }
