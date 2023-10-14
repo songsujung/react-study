d63434679eeb6714bb73922bd84d0889f6f9904b
sj <sujung033131@gmail.com>
2023-10-14 22:22:32 +0900

    1014 Router, Link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/hello' element={<h1>Hello!</h1>}></Route>
+      </Routes>
+      <Routes>
+        <Route path='/movie' element={<Detail />}></Route>
+      </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+    );
 }
+
 export default App;
+
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
+// Link를 사용하면 페이지 전체를 새로고침하지 않고 페이지 이동이 이뤄짐(빠르게 반응하는 것처럼 보여짐)
 function Movie({coverImg, title, summary, genres}) {
     return <div>
     <img src={coverImg} />
-    <h2>{title}</h2>
+    <h2><Link to="/movie">{title}</Link></h2>
     <p>{summary}</p>
     <ul>
       {genres.map((g) => (
